3893e0ca23cf73fc61475f635e5a66f4e695526b
elshan.eth <elshan.eth>
2022-08-15 23:16:52 +0400

    add double key case

--- reward/test.sh
+++ reward/test.sh
@@ -26,9 +26,10 @@ PUB_KEY_HASH=$(echo "$PUB_KEY" | xxd -r -p | (sha3sum -a Keccak256 -t || true) |
 RANDOM_ETH_ADDRESS=$(echo "$PUB_KEY_HASH" | xxd -r -p | xxd -p -c 20 -s 12)
 
 USERS=("user_1" "user_2" "user_3")
-TESTS=(test_generate test_proof)
+USER_WITH_DOUBLE_KEY="user_3"
+TESTS=(test_generate test_proof test_double_key)
 
-if_error_exists() {
+fail_if_error() {
     if ! [ $? -eq 0 ]; then
         echo - ❌ fail$'\n'
         exit 1
@@ -52,7 +53,7 @@ test_generate() {
 
     pushd "$MERKLE_PROOF_TOOL_DIR" >/dev/null # change directory to
     EXPECTED_ROOT=$(node index.js merkle_root "$MERKLE_TREE_FILE")
-    if_error_exists
+    fail_if_error
     popd >/dev/null # we're back to WORK_DIR
 
     if [ "$MERKLE_ROOT" != "$EXPECTED_ROOT" ]; then
@@ -77,10 +78,10 @@ verify_sign() {
     VERIFICATION_RESULT=$(echo "$HASH" | xxd -r -p | openssl pkeyutl -verify -keyform DER -inkey "$TEMP_ETH_KEY_DER" -sigfile "$SIGN_FILE")
 
     TEMP_PUB_KEY=$(openssl ec -pubout -outform der -inform der -in "$TEMP_ETH_KEY_DER" 2>/dev/null | xxd -s 24 -p -c 64)
-    if_error_exists
+    fail_if_error
 
     TEMP_PUB_KEY_HASH=$(echo "$TEMP_PUB_KEY" | xxd -r -p | (sha3sum -a Keccak256 -t || true) | awk -F $'\xC2\xA0' '{ print $1 }')
-    if_error_exists
+    fail_if_error
 
     TEMP_ETH_ADDR=$(echo "$TEMP_PUB_KEY_HASH" | xxd -r -p | xxd -p -c 20 -s 12)
 
@@ -89,37 +90,43 @@ verify_sign() {
     fi
 }
 
-parse_proof_out() {
-    OUT="$1,"
-    PARSED_OUT=()
-    while [[ $OUT ]]; do
-        PARSED_OUT+=("${OUT%%","*}")
-        OUT=${OUT#*","}
-    done
-
-    declare -a PARSED_OUT
-}
-
 test_proof_by_user() {
     USER=$1
+    KEY=$USER
+    if [ $# -gt 2 ]; then
+        KEY=$2
+    fi
     pushd "$TEST_DIR" >/dev/null
     PROOF_SH_RESULT=$($PROOF_SH "$USER" ${RANDOM_ETH_ADDRESS} "${TEST_DIR}/$USER" | tail -1)
-    if_error_exists
+    fail_if_error
     popd >/dev/null
 
-    parse_proof_out $PROOF_SH_RESULT
+    PARSED_OUT=(${PROOF_SH_RESULT//,/ })
     SIGNATURE=${PARSED_OUT[2]}
     ADDRESS=${PARSED_OUT[1]}
     verify_sign $ADDRESS $SIGNATURE
-    if_error_exists
+    fail_if_error
 }
 
 test_proof() {
     for user in "${USERS[@]}"; do
         echo "- test user: $user"
         test_proof_by_user $user
-        if_error_exists
+        fail_if_error
     done
+}
+
+test_double_key() {
+    test_proof_by_user $USER_WITH_DOUBLE_KEY $USER3_SECOND_KEY_NAME
+    fail_if_error
+
+    MERKLE_TREE=$(cat $WORK_DIR/merkle_tree)
+
+    MERKLE_TREE=(${MERKLE_TREE//n/ })
+
+    if [ ${#MERKLE_TREE[@]} != 3 ]; then
+        throw "Invalid merkle tree"
+    fi
 }
 
 init() {
@@ -140,6 +147,13 @@ init() {
         PUB_KEY=$(cat "$TEST_DIR/$user.pub")
         echo -e "$user,$(echo $PUB_KEY | awk -F ' ' '{print $1" "$2}')" >>$REWARDED_KEYS_FULL_PATH
     done
+
+    USER3_SECOND_KEY_NAME="${USER_WITH_DOUBLE_KEY}_second"
+
+    ssh-keygen -t rsa -C "$USER_WITH_DOUBLE_KEY" -f "$TEST_DIR/$USER3_SECOND_KEY_NAME" -N ""
+
+    PUB_KEY=$(cat "$TEST_DIR/$USER3_SECOND_KEY_NAME.pub")
+    echo -e "user_3,$(echo $PUB_KEY | awk -F ' ' '{print $1" "$2}')" >>$REWARDED_KEYS_FULL_PATH
 }
 
 echo "Initialization..."
@@ -154,7 +168,7 @@ for test in "${TESTS[@]}"; do
     $test
     set -o errexit
 
-    if_error_exists
+    fail_if_error
 
     echo - ✅ success$'\n'
 done
