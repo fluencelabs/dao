bb29c494fc028e5a65f0b0bb66b05ddc1e93c61e
elshan.eth <elshan.eth>
2022-08-29 23:00:09 +0400

    install utils update

--- reward/sh/install_utils.sh
+++ reward/sh/install_utils.sh
@@ -8,7 +8,16 @@ set -o errexit -o nounset -o pipefail
 
 cargo install --force sha3sum
 
-AGE_URL="https://github.com/FiloSottile/age/releases/latest/download/age-v1.0.0-linux-amd64.tar.gz"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ $(uname -m) == 'arm64' ]]; then
+        AGE_URL="https://github.com/FiloSottile/age/releases/latest/download/age-v1.0.0-darwin-arm64.tar.gz"
+    else
+        AGE_URL="https://github.com/FiloSottile/age/releases/latest/download/age-v1.0.0-darwin-amd64.tar.gz"
+    fi
+else
+    AGE_URL="https://github.com/FiloSottile/age/releases/latest/download/age-v1.0.0-linux-amd64.tar.gz"
+fi
+
 BIN_DIR="/usr/local/bin"
 curl -Lo age.tar.gz $AGE_URL
 tar xf age.tar.gz
