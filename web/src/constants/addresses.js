03fad74ac8103a4c1efbb22c9f337c37068ce4aa
elshan_eth <raiman.elshan@gmail.com>
2024-02-21 16:35:07 +0100

    add sepolia. drop other testnets

--- web/src/constants/addresses.js
+++ web/src/constants/addresses.js
@@ -1,20 +1,6 @@
 export const governanceContracts = {
-    "rinkeby": {
-        "token": "0xF749C8d16B8892386a4D375E3D07f85E92537D83",
-        "timelock": "0x4CC80D5BF49E0c6E4BdDd6CDFD1caCB6Aa6FdC59",
-        "alpha": "0xA56A51BEb05a6Ec32625DfB8868c21Ab27bD86d5",
-        "devRewardDistributor": "0x138419d600921C452df32AE40a17b366B30411f2"
-    },
-    "kovan": {
-        "token": "0xF749C8d16B8892386a4D375E3D07f85E92537D83",
-        "timelock": "0x4CC80D5BF49E0c6E4BdDd6CDFD1caCB6Aa6FdC59",
-        "alpha": "0xA56A51BEb05a6Ec32625DfB8868c21Ab27bD86d5",
-        "devRewardDistributor": "0x138419d600921C452df32AE40a17b366B30411f2"
-    },
-    "hardhat": {
-        "token": "0xF749C8d16B8892386a4D375E3D07f85E92537D83",
-        "timelock": "0x4CC80D5BF49E0c6E4BdDd6CDFD1caCB6Aa6FdC59",
-        "alpha": "0xA56A51BEb05a6Ec32625DfB8868c21Ab27bD86d5",
-        "devRewardDistributor": "0x138419d600921C452df32AE40a17b366B30411f2"
-    },
-}
+  sepolia: {
+    token: "0x1e554D8f3Bd07D27ef47DD4571a16CDA74c3b731",
+    devRewardDistributor: "0x9b9daa01a290786D520013FfcB3933277A908a3A",
+  },
+};
