3ac0668a4e8a0bebb355096c94f7b9073bf68b83
elshan_eth <raiman.elshan@gmail.com>
2024-02-27 18:43:25 +0100

    create test env

--- contracts/deploy-mainnet/0004_DevReward.ts
+++ contracts/deploy-mainnet/0004_DevReward.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import "hardhat-deploy";
-import { HOURS, MINUTES } from "../utils/time";
+import { MINUTES, MONTH } from "../utils/time";
 import { ethers } from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -14,81 +14,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     from: deployer,
     contract: "DevRewardDistributor",
     args: [
-      token.address,
-      deployer,
-      "0x82e1cd2ef5365e6db5b707763079758302a04bb39e2b5199f460fc316b74890a",
-      2 * HOURS,
-      10 * MINUTES,
-      ethers.utils.parseEther("1000"),
-      20 * HOURS,
-      deployer,
-      ethers.utils.parseEther("1000000000"),
+      token.address, // _token
+      deployer, // executor
+      "0x9054c3420799d2857b3706ca310823473c3b76dd666412134952aeba279e888a",
+      30 * MINUTES, // harvestPeriod
+      5 * MINUTES, // _lockupPeriod
+      ethers.utils.parseEther("5000"), // initialReward
+      12 * MONTH, // claimPeriod
+      deployer, // cenceler
+      ethers.utils.parseEther("50000000"), // maxClaimedSupply
     ],
     waitConfirmations: 1,
   });
 
   console.log(`DevRewardDistributor deployed to ${deployResult.address}\n`);
-
-  // const teamVestingResult = await hre.deployments.deploy("TeamVesting", {
-  //   from: deployer,
-  //   contract: "VestingWithVoting",
-  //   args: [
-  //     token.address,
-  //     "Fluence Token (Locked, Voting)",
-  //     "FLT-LV #666",
-  //     20 * HOURS,
-  //     20 * HOURS,
-  //     [
-  //       "0x198691769280d07706a9c85B30565E928F8A9025",
-  //       "0x074e67fF7cE5A91055E42AdA692289aE2225Be06",
-  //     ],
-  //     [
-  //       ethers.utils.parseEther("100000000"),
-  //       ethers.utils.parseEther("100000000"),
-  //     ],
-  //   ],
-  //   waitConfirmations: 1,
-  // });
-
-  // console.log(`TeamVesting deployed to ${teamVestingResult.address}\n`);
-
-  // const deployExecutorResult = await hre.deployments.deploy("Executor", {
-  //   from: deployer,
-  //   proxy: {
-  //     proxyContract: "ERC1967Proxy",
-  //     proxyArgs: ["{implementation}", "{data}"],
-  //     execute: {
-  //       methodName: "initialize",
-  //       args: [180],
-  //     },
-  //   },
-  //   waitConfirmations: 1,
-  // });
-
-  // console.log(`Executor deployed to ${deployExecutorResult.address}`);
-
-  // const deployDAOResult = await hre.deployments.deploy("Governor", {
-  //   from: deployer,
-  //   proxy: {
-  //     proxyContract: "ERC1967Proxy",
-  //     proxyArgs: ["{implementation}", "{data}"],
-  //     execute: {
-  //       methodName: "initialize",
-  //       args: [
-  //         token.address,
-  //         teamVestingResult.address,
-  //         deployExecutorResult.address,
-  //         1, // quorum
-  //         Math.floor((10 * MINUTES) / 12), // voting delay
-  //         Math.floor((10 * MINUTES) / 12), // voting period
-  //         ethers.utils.parseEther("1000"), // proposal threshold
-  //       ],
-  //     },
-  //   },
-  //   waitConfirmations: 1,
-  // });
-
-  // console.log(`Governor deployed to ${deployDAOResult.address}`);
 };
 
 export default func;
--- web/src/constants/addresses.js
+++ web/src/constants/addresses.js
@@ -1,6 +1,6 @@
 export const governanceContracts = {
   sepolia: {
     token: "0x1e554D8f3Bd07D27ef47DD4571a16CDA74c3b731",
-    devRewardDistributor: "0x56dCb8dDD86B970a5229c4B0DB8f5B179A9a651D",
+    devRewardDistributor: "0xFC3D7144d302Bc19B2363Fb28F3061a14255996d",
   },
 };
