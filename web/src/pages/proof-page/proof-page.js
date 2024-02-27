f6cbca1874c90a51438a32325f171e0790a1684a
elshan_eth <raiman.elshan@gmail.com>
2024-02-27 23:33:16 +0100

    remove install cargo

--- web/src/pages/proof-page/proof-page.js
+++ web/src/pages/proof-page/proof-page.js
@@ -261,11 +261,6 @@ const ProofPage = () => {
                     </div>
                     For bash script:
                     <div className={styles.dashboard__textarea}>
-                      <p className={styles.paragraph}>
-                        {" "}
-                        Instal cargo
-                        (https://doc.rust-lang.org/cargo/getting-started/installation.html)
-                      </p>
                       <p className={styles.paragraph}>./install.sh</p>
                       <p className={styles.paragraph}>./proof-sh/proof.sh</p>
                     </div>
