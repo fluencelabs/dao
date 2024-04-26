9e15129e837e2443a7075ce5804c2d6f8c439054
Dima <crystalbit@mail.ru>
2024-04-26 18:31:05 +0200

    remove unused

--- web/src/pages/claim-flt-page/helpers.js
+++ web/src/pages/claim-flt-page/helpers.js
@@ -1,10 +1,3 @@
-export const TOKENS = {
-  "1": "0x6081d7F04a8c31e929f25152d4ad37c83638C62b",
-  "31337": "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-  "11155111": "0x35a0a84E0DDA0587794E1FB19543C65926040E03",
-  "43113": "0x44f9a4d2441efBf23646928CB6F27219f2b79A1a",
-};
-
 export function formatSeconds(seconds) {
   // Calculate days, hours, minutes, and remaining seconds
   const days = Math.floor(seconds / 86400);
